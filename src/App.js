bf64a60e8ef1ba9f1252227d2a2cf7a431415a27
LABIT <labit@uit.edu>
2019-11-16 17:16:42 +0500

    done

--- src/App.js
+++ src/App.js
@@ -17,21 +17,36 @@ export class App extends Component {
   // }
   // handleChangeEnd(event) {
   //   this.setState({ endDate: event.target.endDate });
-  // }
-  
+  calculate() {
+    this.setState(
+
+      {
+        startD = this.state.startDate,
+        endD = this.state.endDate,
+        sub = startD - endD,
+        divide = sub / 11,
+        multi = divide * 11,
+        total = sub - multi,
+        remaning = total
+      }
+    )
+    return remaning;
+  }
 
   render() {
-    console.log (this.state.startDate);
-    console.log (this.state.endDate);
+    console.log(this.state.startDate);
+    console.log(this.state.endDate);
     return (
       <div>
         {/* <input type="date" name="start" onChange={this.handleChangeStart}>
         </input>
         <input type="date" name="end" onChange={this.handleChangeEnd}>
         </input> */}
-        <input type="date" onChange={(event) => this.setState({startDate: event.target.value})}/>
-        <input type="date" onChange={(event) => this.setState({endDate: event.target.value})}/>
+        <input type="date" onChange={(event) => this.setState({ startDate: event.target.value })} />
+        <input type="date" onChange={(event) => this.setState({ endDate: event.target.value })} />
+
         <button >Result</button>
+        <h1>Remaning Leave <span>0</span></h1>
       </div>
     );
   }
