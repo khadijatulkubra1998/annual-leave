488e75089c0ea77c1d3326ee5875684336a813e5
Khadija Tul Kubra <khadijatulkubra504@gmail.com>
2019-11-18 23:41:20 +0500

    not working

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ export class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      newDate: "",
       startDate: "",
       endDate: "",
+      startD:"",
+      endD:"",
+      sub:"",
+      divide:"",
+      multi:"",
+      total:"",
+      remaining:""
     }
     // this.handleChangeStart = this.handleChangeStart.bind(this)
     // this.handleChangeEnd = this.handleChangeEnd.bind(this)
@@ -19,16 +25,16 @@ export class App extends Component {
   //   this.setState({ endDate: event.target.endDate });
   calculate() {
     this.setState(
-
       {
-        startD = this.state.startDate,
-        endD = this.state.endDate,
-        sub = this.state.startD - this.state.endD,
-        divide = this.state.sub / 11,
-        multi = this.state.divide * 11,
-        total = this.state.sub - multi,
-        remaning = this.state.total
+        startD : this.state.startDate,
+        endD : this.state.endDate,
+        sub : this.state.startD - this.state.endD,
+        divide : this.state.sub / 11,
+        multi : this.state.divide * 11,
+        total : this.state.sub - this.state.multi,
+        remaning : this.state.total
       }
+      // console.log(remaining);
     )
   }
 
@@ -37,14 +43,11 @@ export class App extends Component {
     console.log(this.state.endDate);
     return (
       <div>
-        {/* <input type="date" name="start" onChange={this.handleChangeStart}>
-        </input>
-        <input type="date" name="end" onChange={this.handleChangeEnd}>
-        </input> */}
+       
         <input type="date" onChange={(event) => this.setState({ startDate: new Date(event.target.value).getDate()})} />
         <input type="date" onChange={(event) => this.setState({ endDate: new Date(event.target.value).getDate() })} />
 
-        <button onClick{calculate()}>Result</button>
+        <button onClick={this.calculate()}>Result </button>
         <h1>Remaning Leave <span>{this.state.remaining}</span></h1>
       </div>
     );
