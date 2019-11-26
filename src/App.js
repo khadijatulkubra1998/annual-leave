40d27d7c574cf2b31c13884aeb46ba9d1fff1853
Khadija Tul Kubra <49521166+khadijatulkubra1998@users.noreply.github.com>
2019-11-26 18:17:08 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
 import { DatePicker } from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
 var moment = require('moment');
 moment().format();
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
 export class App extends Component {
   
   constructor(props) {
@@ -40,88 +43,9 @@ export class App extends Component {
         }
       )
     }
-class DateRange extends React.Component {
-  state = {
-    startValue: null,
-    endValue: null,
-    endOpen: false,
-  };
 
-  disabledStartDate = startValue => {
-    const { endValue } = this.state;
-    if (!startValue || !endValue) {
-      return false;
-    }
-    return startValue.valueOf() > endValue.valueOf();
-  };
 
-  disabledEndDate = endValue => {
-    const { startValue } = this.state;
-    if (!endValue || !startValue) {
-      return false;
-    }
-    return endValue.valueOf() <= startValue.valueOf();
-  };
 
-  onChange = (field, value) => {
-    this.setState({
-      [field]: value,
-    });
-  };
-
-  onStartChange = value => {
-    this.onChange('startValue', value);
-  };
-
-  onEndChange = value => {
-    this.onChange('endValue', value);
-  };
-
-  handleStartOpenChange = open => {
-    if (!open) {
-      this.setState({ endOpen: true });
-    }
-  };
-
-  handleEndOpenChange = open => {
-    this.setState({ endOpen: open });
-  };
-
-  render() {
-    const { startValue, endValue, endOpen } = this.state;
-    return (
-      <div>
-        <DatePicker
-        className="btn btn-outline-light"
-          disabledDate={this.disabledStartDate}
-          showTime
-          format="YYYY-MM-DD HH:mm:ss"
-          value={startValue}
-          placeholder="Start"
-          onChange={this.onStartChange}
-          onOpenChange={this.handleStartOpenChange}
-          onChange={(event) => this.setState({ startDate: event.target.value })} 
-        />
-        <DatePicker
-        className="btn btn-outline-light"
-          disabledDate={this.disabledEndDate}
-          showTime
-          format="YYYY-MM-DD HH:mm:ss"
-          value={endValue}
-          placeholder="End"
-          onChange={this.onEndChange}
-          open={endOpen}
-          onOpenChange={this.handleEndOpenChange}
-          onChange={(event) => this.setState({ endDate: event.target.value })}
-        />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<DateRange />, mountNode);
-
-// export class App extends Component {
   
 //   constructor(props) {
 //     super(props)
@@ -165,23 +89,27 @@ ReactDOM.render(<DateRange />, mountNode);
 //       }
 //     )
 //   }
-//   render() {
-//     // console.log(this.state.startDate);
-//     // console.log(this.state.endDate);
-    
-//     return (
-//       <div className="App">
-//           <h1 >Remaining Leave <span>"{this.state.remaining}"</span></h1>
-//         {/* <label  className="btn btn-info">Start Date</label>
-//         <input type="date" className="btn btn-outline-light" onChange={(event) => this.setState({ startDate: event.target.value })} />
-//         <label  className="btn btn-info"> End Date </label>
-//         <input type="date"   className="btn btn-outline-light"  onChange={(event) => this.setState({ endDate: event.target.value })} /> */}
-//         <Button  variant="outline-warning" onClick={() => this.calculate()}>Result</Button>
-       
-//       </div>
-//     );
-//   }
-// }
 
-// export default App;
+
+  render() {
+    console.log(this.state.startDate);
+    console.log(this.state.endDate);
+    
+    return (
+      <div className="App">
+          <h1 >Remaining Leave <span>"{this.state.remaining}"</span></h1>
+        <label  className="btn btn-info">Start Date</label>
+        <DatePicker onChange={(date) => this.setState({startDate: date})}  placeholder="Select date" />
+        {/* <input type="date" className="btn btn-outline-light" onChange={(event) => this.setState({ startDate: event.target.value })} /> */}
+        <label  className="btn btn-info"> End Date </label>
+        <DatePicker onChange={(date) => this.setState({endDate: date})}  placeholder="Select date" />
+        {/* <input type="date" className="btn btn-outline-light"  onChange={(event) => this.setState({ endDate: event.target.value })} /> */}
+        <Button  variant="outline-warning" onClick={() => this.calculate()}>Result</Button>
+       
+      </div>
+    );
+  }
+}
+
+export default App;
 
--- src/script.js
+++ src/script.js
@@ -1,96 +1,116 @@
-// let d = new Date();
-// let startDate = document.querySelector("input").value=d.getDate();
-// let endDate = document.querySelector("input").value=d.getDate();
-// let sub = startDate - endDate;
-// let divide = sub/11;
-// let multi = divide * 11;
-// let total = sub-multi;
-// let remaning = total;
+// // let d = new Date();
+// // let startDate = document.querySelector("input").value=d.getDate();
+// // let endDate = document.querySelector("input").value=d.getDate();
+// // let sub = startDate - endDate;
+// // let divide = sub/11;
+// // let multi = divide * 11;
+// // let total = sub-multi;
+// // let remaning = total;
 
-// var endDate = moment("2019-11-19"); //todays date
-// var startDate = moment("2019-11-1"); // another date
-// var duration = moment.duration(endDate.diff(startDate));
-// var days = duration.asDays();
-// console.log(days)
-import { DatePicker } from 'antd';
+// // var endDate = moment("2019-11-19"); //todays date
+// // var startDate = moment("2019-11-1"); // another date
+// // var duration = moment.duration(endDate.diff(startDate));
+// // var days = duration.asDays();
+// // console.log(days)
+// import { DatePicker } from 'antd';
 
-class DateRange extends React.Component {
-  state = {
-    startValue: null,
-    endValue: null,
-    endOpen: false,
-  };
+// class DateRange extends React.Component {
+//   state = {
+//     startValue: null,
+//     endValue: null,
+//     endOpen: false,
+//   };
 
-  disabledStartDate = startValue => {
-    const { endValue } = this.state;
-    if (!startValue || !endValue) {
-      return false;
-    }
-    return startValue.valueOf() > endValue.valueOf();
-  };
+//   disabledStartDate = startValue => {
+//     const { endValue } = this.state;
+//     if (!startValue || !endValue) {
+//       return false;
+//     }
+//     return startValue.valueOf() > endValue.valueOf();
+//   };
 
-  disabledEndDate = endValue => {
-    const { startValue } = this.state;
-    if (!endValue || !startValue) {
-      return false;
-    }
-    return endValue.valueOf() <= startValue.valueOf();
-  };
+//   disabledEndDate = endValue => {
+//     const { startValue } = this.state;
+//     if (!endValue || !startValue) {
+//       return false;
+//     }
+//     return endValue.valueOf() <= startValue.valueOf();
+//   };
 
-  onChange = (field, value) => {
-    this.setState({
-      [field]: value,
-    });
-  };
+//   onChange = (field, value) => {
+//     this.setState({
+//       [field]: value,
+//     });
+//   };
 
-  onStartChange = value => {
-    this.onChange('startValue', value);
-  };
+//   onStartChange = value => {
+//     this.onChange('startValue', value);
+//   };
 
-  onEndChange = value => {
-    this.onChange('endValue', value);
-  };
+//   onEndChange = value => {
+//     this.onChange('endValue', value);
+//   };
 
-  handleStartOpenChange = open => {
-    if (!open) {
-      this.setState({ endOpen: true });
-    }
-  };
+//   handleStartOpenChange = open => {
+//     if (!open) {
+//       this.setState({ endOpen: true });
+//     }
+//   };
 
-  handleEndOpenChange = open => {
-    this.setState({ endOpen: open });
-  };
+//   handleEndOpenChange = open => {
+//     this.setState({ endOpen: open });
+//   };
 
-  render() {
-    const { startValue, endValue, endOpen } = this.state;
-    return (
-      <div>
-        <DatePicker
-        className="btn btn-outline-light"
-          disabledDate={this.disabledStartDate}
-          showTime
-          format="YYYY-MM-DD HH:mm:ss"
-          value={startValue}
-          placeholder="Start"
-          onChange={this.onStartChange}
-          onOpenChange={this.handleStartOpenChange}
-          onChange={(event) => this.setState({ startDate: event.target.value })} 
-        />
-        <DatePicker
-        className="btn btn-outline-light"
-          disabledDate={this.disabledEndDate}
-          showTime
-          format="YYYY-MM-DD HH:mm:ss"
-          value={endValue}
-          placeholder="End"
-          onChange={this.onEndChange}
-          open={endOpen}
-          onOpenChange={this.handleEndOpenChange}
-          onChange={(event) => this.setState({ endDate: event.target.value })}
-        />
-      </div>
-    );
-  }
-}
+//   render() {
+//     const { startValue, endValue, endOpen } = this.state;
+//     return (
+//       <div>
+//         <DatePicker
+//         className="btn btn-outline-light"
+//           disabledDate={this.disabledStartDate}
+//           showTime
+//           format="YYYY-MM-DD HH:mm:ss"
+//           value={startValue}
+//           placeholder="Start"
+//           onChange={this.onStartChange}
+//           onOpenChange={this.handleStartOpenChange}
+//           onChange={(event) => this.setState({ startDate: event.target.value })} 
+//         />
+//         <DatePicker
+//         className="btn btn-outline-light"
+//           disabledDate={this.disabledEndDate}
+//           showTime
+//           format="YYYY-MM-DD HH:mm:ss"
+//           value={endValue}
+//           placeholder="End"
+//           onChange={this.onEndChange}
+//           open={endOpen}
+//           onOpenChange={this.handleEndOpenChange}
+//           onChange={(event) => this.setState({ endDate: event.target.value })}
+//         />
+//       </div>
+//     );
+//   }
+// }
 
-ReactDOM.render(<DateRange />, mountNode);
+// ReactDOM.render(<DateRange />, mountNode);
+// import { DatePicker } from 'antd';
+
+// const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
+// function onChange(date, dateString) {
+//   console.log(date, dateString);
+// }
+
+// ReactDOM.render(
+//   <div>
+//     <DatePicker onChange={onChange} />
+//     <br />
+//     <MonthPicker onChange={onChange} placeholder="Select month" />
+//     <br />
+//     <RangePicker onChange={onChange} />
+//     <br />
+//     <WeekPicker onChange={onChange} placeholder="Select week" />
+//   </div>,
+//   mountNode,
+// );
