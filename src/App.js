1d66b39bb9ed88b5c1518c63bc6bb71e5f76b76d
Khadija Tul Kubra <49521166+khadijatulkubra1998@users.noreply.github.com>
2019-12-04 17:13:50 +0500

    half complete

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ moment().format();
 const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
 
 export class App extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
@@ -20,47 +20,55 @@ export class App extends Component {
       divide: "",
       multi: "",
       total: "",
-      remaining: ""
+      remaining: "",
+      days: ""
     }
-   }
-    calculate() {
-      let endD = moment(this.state.endDate); //todays date
-      let startD = moment(this.state.startDate); // another date
-      let duration = moment.duration(endD.diff(startD));
-      let days = duration.asDays();
-      let divide = Math.round(days / 11);
-      let multi = Math.round(divide * 11);
-      let total = days - multi
-      this.setState(
-        {
-          remaining: total
-        }, () => {
-          console.log(days);
-          console.log(divide);
-          console.log(multi);
-          console.log(total);
-          console.log(this.state.remaining)
-        }
-      )
-    }
+  }
+  calculate() {
+    let endD = moment(this.state.endDate); //todays date
+    let startD = moment(this.state.startDate); // another date
+    let duration = moment.duration(endD.diff(startD));
+    let days = duration.asDays();
+    let divide = parseInt(days / 11);
+    let multi = parseInt(divide * 11);
+    let total = parseInt(days - multi);
+    this.setState(
+      {
+        days: days,
+        remaining: total,
+        multi: multi,
+        divide: divide,
+      }, () => {
+        console.log(days);
+        console.log(divide);
+        console.log(multi);
+        console.log(total);
+        console.log(this.state.remaining)
+      }
+    )
+  }
 
 
 
   render() {
     console.log(this.state.startDate);
     console.log(this.state.endDate);
-    
+
     return (
       <div className="App">
-          <h1 >Remaining Leave <span>"{Math.round(this.state.remaining)}"</span></h1>
-        <label  className="btn btn-info">Start Date</label>
-        <DatePicker onChange={(date) => this.setState({startDate: date})}  placeholder="Select date" />
+        <h1>Remaining Days <span>"{this.state.remaining}"</span></h1>
+        <p>Total Days: {parseInt(this.state.days)}</p>
+        <p>Divide: {parseInt(this.state.days)} / 11</p>
+        <p>Earnad Leaves: {this.state.divide}</p>
+        <p>Days: {this.state.multi}</p>
+        <label className="btn btn-info">Start Date</label>
+        <DatePicker onChange={(date) => this.setState({ startDate: date })} placeholder="Select date" />
         {/* <input type="date" className="btn btn-outline-light" onChange={(event) => this.setState({ startDate: event.target.value })} /> */}
-        <label  className="btn btn-info"> End Date </label>
-        <DatePicker onChange={(date) => this.setState({endDate: date})}  placeholder="Select date" />
+        <label className="btn btn-info"> End Date </label>
+        <DatePicker onChange={(date) => this.setState({ endDate: date })} placeholder="Select date" />
         {/* <input type="date" className="btn btn-outline-light"  onChange={(event) => this.setState({ endDate: event.target.value })} /> */}
-        <Button  variant="outline-warning" onClick={() => this.calculate()}>Result</Button>
-       
+        <Button variant="outline-warning" onClick={() => this.calculate()}>Result</Button>
+
       </div>
     );
   }
