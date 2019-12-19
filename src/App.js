afeceb73d83f08e8f9e6e733cd59b2edfc039611
Khadija Tul Kubra <49521166+khadijatulkubra1998@users.noreply.github.com>
2019-12-19 12:55:29 +0500

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
-import { DatePicker } from 'antd';
+import { DatePicker, Modal } from 'antd';
 import 'antd/dist/antd.css';
 import './App.css';
 var moment = require('moment');
@@ -21,7 +21,8 @@ export class App extends Component {
       multi: "",
       total: "",
       remaining: "",
-      days: ""
+      days: "",
+      visible: false
     }
   }
   calculate() {
@@ -46,28 +47,60 @@ export class App extends Component {
         console.log(this.state.remaining)
       }
     )
+
   }
 
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  };
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  };
 
+  result =() => {
+    this.calculate()
+    this.showModal()
+  }
   render() {
     console.log(this.state.startDate);
     console.log(this.state.endDate);
 
     return (
       <div className="App">
-        <h1>Remaining Days <span>"{this.state.remaining}"</span></h1>
-        <p>Total Days: {parseInt(this.state.days)}</p>
-        <p>Divide: {parseInt(this.state.days)} / 11</p>
-        <p>Earnad Leaves: {this.state.divide}</p>
-        <p>Days: {this.state.multi}</p>
+        <h1>Remaining Days calculate</h1>
+        {/* <h3>About: <p>This application is maade for calculate remaining days of leaves. This is for college administration with specific calculation.</p> </h3> */}
+        <p>About: This application is made for calculate remaining days of leaves. This is for college administration with specific calculation. </p>
         <label className="btn btn-info">Start Date</label>
         <DatePicker onChange={(date) => this.setState({ startDate: date })} placeholder="Select date" />
         {/* <input type="date" className="btn btn-outline-light" onChange={(event) => this.setState({ startDate: event.target.value })} /> */}
         <label className="btn btn-info"> End Date </label>
         <DatePicker onChange={(date) => this.setState({ endDate: date })} placeholder="Select date" />
         {/* <input type="date" className="btn btn-outline-light"  onChange={(event) => this.setState({ endDate: event.target.value })} /> */}
-        <Button variant="outline-warning" onClick={() => this.calculate()}>Result</Button>
+        <Button variant="outline-warning" onClick={() => this.result()}>Result</Button>
+        <Modal
+          title="Result"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+        >
+          <p>Total Days: {parseInt(this.state.days)}</p>
+          <p>Divide: {parseInt(this.state.days)} / 11</p>
+          <p>Earnad Leaves: {this.state.divide}</p>
+          <p>Days: {this.state.multi}</p>
+          <p>Remaining Days <span>"{this.state.remaining}"</span></p>
+        </Modal>
 
       </div>
     );
